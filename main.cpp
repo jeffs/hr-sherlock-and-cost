56a197734d5fe319ce047026d5c16f05139f891d
Jeff Schwab <jeff@orchardplatform.com>
2017-01-01 18:20:22 -0500

    Solved.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 
 #ifndef DEBUG
@@ -11,7 +11,7 @@
 #   define DUMP(x) std::clog << #x " = " << nlohmann::json(x) << '\n'
 #endif
 
-namespace {
+namespace jeff {
 
 using value_type = int;
 using array_type = std::vector<value_type>;
@@ -33,6 +33,22 @@ bool operator<(node a, node b) {
         || (a.position == b.position && a.value < b.value);
 }
 
+bool operator==(node a, node b) {
+    return a.position == b.position && a.value == b.value;
+}
+
+}
+namespace std {
+    template <>
+    struct hash<jeff::node> {
+        std::size_t operator()(jeff::node a) const {
+            return hash<jeff::value_type const*>{}(&*a.position)
+                ^ hash<jeff::value_type>{}(a.value);
+        }
+    };
+}
+namespace jeff {
+
 template <class T>
 T read()
 {
@@ -45,7 +61,7 @@ T read()
 value_type from_node(
         node source,
         iterator e,
-        std::map<node, value_type>& memo)
+        std::unordered_map<node, value_type>& memo)
 {
     if (!memo.count(source)) {
         auto from_edge = [source, e, &memo](node target) {
@@ -62,7 +78,7 @@ value_type max_cost(array_type const& values)
 {
     return values.size() < 2 ? 0 : [&values] {
         iterator b = begin(values), e = end(values);
-        std::map<node, value_type> memo;
+        std::unordered_map<node, value_type> memo;
         return std::max(
                 from_node({b, 1}, e, memo),
                 from_node({b, *b}, e, memo));
@@ -73,6 +89,7 @@ value_type max_cost(array_type const& values)
 
 int main()
 {
+    using namespace jeff;
     for (int t = read<int>(); t--;) {
         DUMP(t);
         array_type b(read<array_type::size_type>());
